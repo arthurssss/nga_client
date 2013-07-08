1c2bbf7aeaad5c71c5de638ea4313fc9a347c009
arthur <arthursssssss@gmail.com>
2013-07-08 23:41:38 +0800

    数据库

---
+++ NGAClient/src/com/arthur/ngaclient/util/DBHelper.java
@@ -0,0 +1,44 @@
+package com.arthur.ngaclient.util;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+	private static final String DB_NAME = "nga_client.db";
+	private static final int DB_VERSION = 1;
+
+	public DBHelper(Context context) {
+		super(context, DB_NAME, null, DB_VERSION);
+	}
+
+	public DBHelper(Context context, String name, CursorFactory factory,
+			int version, DatabaseErrorHandler errorHandler) {
+		super(context, name, factory, version, errorHandler);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("create table board(id INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ "name varchar(30), url varchar(20), src INTEGER)");
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+
+	@Override
+	public void onOpen(SQLiteDatabase db) {
+		super.onOpen(db);
+	}
+
+	@Override
+	public synchronized void close() {
+		super.close();
+	}
+
+}
