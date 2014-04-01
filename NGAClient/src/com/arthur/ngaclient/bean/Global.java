3cdf69793c56b55135ae14d75afc7c1c138f0b4b
arthur <arthursssssss@gmail.com>
2014-04-01 13:13:22 +0800

    修改域名

--- NGAClient/src/com/arthur/ngaclient/bean/Global.java
+++ NGAClient/src/com/arthur/ngaclient/bean/Global.java
@@ -2,5 +2,5 @@ package com.arthur.ngaclient.bean;
 
 public class Global {
 
-	public final static String SERVER = "http://bbs.ngacn.cc";
+	public final static String SERVER = "http://nga.178.com";
 }
